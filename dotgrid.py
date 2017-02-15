64918888ae3f9f81644d53f38a01d730c46e0ec1
Jon Hallett <jon.hallett@bristol.ac.uk>
2017-02-15 16:36:31 +0000

    Make dotgrid.py pass pep8

--- dotgrid.py
+++ dotgrid.py
@@ -4,28 +4,30 @@ from __future__ import print_function, division
 import sys
 import argparse
 
-default_grid_size = 8.0 # millimetres
+default_grid_size = 8.0  # millimetres
 
-a4_width = 210 # millimetres
-a4_height = 297 # millimetres
+a4_width = 210  # millimetres
+a4_height = 297  # millimetres
 
 point = 25.4/72
 
 page_width = a4_width
 page_height = a4_height
 
-top_margin = 5 # millimetres
-bottom_margin = top_margin # millimetres
-left_margin = 6.5 # millimetres
-right_margin = left_margin # millimetres
-    
+top_margin = 5  # millimetres
+bottom_margin = top_margin  # millimetres
+left_margin = 6.5  # millimetres
+right_margin = left_margin  # millimetres
+
 
 def main():
     set_grid_size_from_argument_or_default()
-    x_grid_size = fit_grid_size_to_printable_length(page_width, left_margin,
-                                                                right_margin)
-    y_grid_size = fit_grid_size_to_printable_length(page_height, bottom_margin,
-                                                                 top_margin)
+    x_grid_size = fit_grid_size_to_printable_length(page_width,
+                                                    left_margin,
+                                                    right_margin)
+    y_grid_size = fit_grid_size_to_printable_length(page_height,
+                                                    bottom_margin,
+                                                    top_margin)
     print_grid_size_fitting_summary(x_grid_size, y_grid_size)
     print(postscript_header(grid_size))
     print(postscript_set_page_size(page_width, page_height))
@@ -41,7 +43,7 @@ def set_grid_size_from_argument_or_default():
                         default=default_grid_size,
                         type=float)
     args = parser.parse_args()
-    
+
     grid_size = args.grid_size
 
 
@@ -75,16 +77,16 @@ def print_postscript_dotgrid_page(x_grid_size, y_grid_size):
     print("0.8 setgray")
 
     for x in frange(left_margin,
-		    page_width - right_margin + 1,
-		    x_grid_size):
+                    page_width - right_margin + 1,
+                    x_grid_size):
         for y in frange(bottom_margin,
-		       	page_height - top_margin + 1,
-		       	y_grid_size):
+                        page_height - top_margin + 1,
+                        y_grid_size):
             x_in_points = truncate_to_3_decimal_points(x / point)
             y_in_points = truncate_to_3_decimal_points(y / point)
             print(postscript_draw_dot(x_in_points, y_in_points))
 
-    print("showpage") 
+    print("showpage")
 
 
 def postscript_draw_dot(x_in_points, y_in_points):
